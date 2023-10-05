4ce2b47d3c5e37eb3382bc84100bdb4db2b60322
PabloChaparroo <pablobautic15@gmail.com>
2023-10-05 09:04:46 -0300

    Molde de spring

---
+++ src/main/java/com/project/API_sin_Paginacion/ApiSinPaginacionApplication.java
@@ -0,0 +1,13 @@
+package com.project.API_sin_Paginacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSinPaginacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSinPaginacionApplication.class, args);
+	}
+
+}
